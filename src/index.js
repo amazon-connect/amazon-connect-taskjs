9e6c36279e42692fb31682f2cd4f244b4a9b2aeb
Sean Colandrea <scoland@users.noreply.github.com>
2020-12-07 15:05:02 -0500

    Add enum (#2)
    Add connect.ReferenceType enum

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import { TaskSessionObject } from './core/taskSession';
-import { createTask } from './core/agentApis';
+import { createTask, ReferenceType } from './core/agentApis';
 
 global.connect = global.connect || {};
 connect.TaskSession = TaskSessionObject;
 
 if (!connect.Agent.prototype.createTask) connect.Agent.prototype.createTask = createTask;
+
+connect.ReferenceType = ReferenceType;
 
 export const TaskSession = TaskSessionObject;
