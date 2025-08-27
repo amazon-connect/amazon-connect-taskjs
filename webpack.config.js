812cde0c8881965a9a284cc3b035b0013f0012f5
Jagadeesha B Y <jagadeey@amazon.com>
2025-08-27 10:47:23 -0700

    Revert "feat: Add comprehensive TypeScript definitions and update to v2.1.0"
    This reverts commit 97032ffc01207fa40359ab32b0c3a5819a5486b9.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index',
@@ -29,14 +28,6 @@ module.exports = {
   plugins: [
     new ESLintPlugin({
       context: 'src',
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: 'src/index.d.ts',
-          to: 'index.d.ts',
-        },
-      ],
     }),
   ],
 };
